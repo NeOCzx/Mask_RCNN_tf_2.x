8f15460fcc267a7c1cf1cea2f668e4cea23b430d
NeOCzx <125035700+NeOCzx@users.noreply.github.com>
2023-02-10 23:19:27 +0800

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -18,11 +18,10 @@ import multiprocessing
 import numpy as np
 import tensorflow.compat.v1 as tf
 # import tensorflow as tf
-import keras
-import keras.backend as K
-import keras.layers as KL
-import keras.engine as KE
-import keras.models as KM
+from tensorflow import keras
+from tensorflow.keras import backend as K
+from tensorflow.keras import layers as KL
+from tensorflow.keras import models as KM
 
 from mrcnn import utils
 
@@ -253,7 +252,7 @@ def clip_boxes_graph(boxes, window):
     return clipped
 
 
-class ProposalLayer(KE.Layer):
+class ProposalLayer(KL.Layer):
     """Receives anchor scores and selects a subset to pass as proposals
     to the second stage. Filtering is done based on anchor scores and
     non-max suppression to remove overlaps. It also applies bounding
@@ -342,7 +341,7 @@ def log2_graph(x):
     return tf.math.log(x) / tf.math.log(2.0)
 
 
-class PyramidROIAlign(KE.Layer):
+class PyramidROIAlign(KL.Layer):
     """Implements ROI Pooling on multiple levels of the feature pyramid.
 
     Params:
@@ -620,7 +619,7 @@ def detection_targets_graph(proposals, gt_class_ids, gt_boxes, gt_masks, config)
     return rois, roi_gt_class_ids, deltas, masks
 
 
-class DetectionTargetLayer(KE.Layer):
+class DetectionTargetLayer(KL.Layer):
     """Subsamples proposals and generates target box refinement, class_ids,
     and masks for each.
 
@@ -783,7 +782,7 @@ def refine_detections_graph(rois, probs, deltas, window, config):
     return detections
 
 
-class DetectionLayer(KE.Layer):
+class DetectionLayer(KL.Layer):
     """Takes classified proposal boxes and their bounding box deltas and
     returns the final detection boxes.
 
@@ -2730,6 +2729,10 @@ class MaskRCNN():
             log(k, v)
         return outputs_np
 
+    def check_cuda_gpu(self):
+        return tf.test.is_built_with_cuda()
+    def unload_model(self):
+        K.clear_session()
 
 ############################################################
 #  Data Formatting
